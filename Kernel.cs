ecc7b29efa3691861af284479b105940ea8bef32
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-10-19 16:27:59 +0100

    Add files via upload

--- Kernel.cs
+++ Kernel.cs
@@ -1,31 +1,67 @@
-﻿using System;
+﻿using Cosmos.System.FileSystem;
+using Cosmos.System.FileSystem.VFS;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Sys = Cosmos.System;
+using IL2CPU.API.Attribs;
+using System.Security.Cryptography;
 
 namespace CosmosKernel1
 {
     public class Kernel : Sys.Kernel
     {
+
+
+        static int locss = 0xb8000;
+        Sys.FileSystem.CosmosVFS fs;
+
         unsafe static void printc(byte b, int loc, byte colors)
         {
             int i = loc;
             byte* fbp = (byte*)i;
             *((byte*)(fbp)) = (byte)b;
             *((byte*)(fbp + 1)) = (byte)colors;
+            locss++;
+            locss++;
         }
         protected override void BeforeRun()
         {
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            fs = new Cosmos.System.FileSystem.CosmosVFS();
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
         }
-        
+
+
+        [ManifestResourceStream(ResourceName = "CosmosKernel1.MY.TXT")]
+        public static byte[] ffile;
+
         protected override void Run()
         {
-            int n = 0;
-            int i = 0xb8000;
-            byte b = 65;
-            for (n=0;n<4000;n=n+2)
-            printc(b, i+n,0xe);
+            Console.Clear();
+            Console.WriteLine("");
+
+            try
+            {
+                foreach (byte byt in ffile)
+                {
+                    printc(byt, locss, 0x60);
+
+                }
+
+               
+                
+
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.ToString());
+            }
+                
+            // Aguarde a entrada do usuário para encerrar
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+            Console.WriteLine("Pressione qualquer tecla para sair...");
             var input = Console.ReadLine();
             
         }
